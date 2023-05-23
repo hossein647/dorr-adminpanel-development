7aaa2cc43aed5ef51d04b241a570a6f37d80d540
hossein <hossein.hedyeh@gmail.com>
2023-05-23 20:34:08 +0330

    implement OnInit in  NavbarComponent and ngOnInit method fo those and init setting property for settings menu in navbar

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/module';
+import { AvatarModule } from 'primeng/avatar'
+import { BadgeModule } from 'primeng/badge';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { Setting } from 'src/app/interfaces/setting.interface';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule, AvatarModule, BadgeModule, OverlayPanelModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  setting: Setting[];
+
+  constructor() {}
+  
+  ngOnInit(): void {
+    this.setting = [
+      { label: 'پروفایل', icon: 'pi pi-user' },
+      { label: 'خروج', icon: 'pi pi-power-off' },
+    ]
+  }
+
+
 
 }
