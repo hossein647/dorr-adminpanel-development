a97be5a2518b90ce36a4d4217d83d7b34db53cc9
hossein <hossein.hedyeh@gmail.com>
2023-05-23 14:32:35 +0330

    create components mainContent && NavbarComponent && SidebarComponent && layoutComponent and make layout site in layout-coponent.html

---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainContentComponent } from './main-content/main-content.component';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [CommonModule, NavbarComponent, SidebarComponent, MainContentComponent],
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutComponent {
+
+}
