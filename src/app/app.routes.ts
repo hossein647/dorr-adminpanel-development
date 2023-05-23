55b2aa0a65be50e4854c3240c5750955cb7b52ce
hossein <hossein.hedyeh@gmail.com>
2023-05-23 14:30:24 +0330

    add lazyload component path with the name LyoutComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '', loadComponent: () => import('./layout/layout.component').then((m) => m.LayoutComponent),
+        children: [],
+        pathMatch: 'full' 
+    }
+];
