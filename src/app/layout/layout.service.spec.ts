4240d0c7d9076e184f1dfdedb85b2e76986109ca
hossein <hossein.hedyeh@gmail.com>
2023-05-24 14:19:52 +0330

    test for showSidebar signal with true value

--- src/app/layout/layout.service.spec.ts
+++ src/app/layout/layout.service.spec.ts
@@ -13,4 +13,8 @@ describe('LayoutService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  
+  it('#showSidebar property should be created', () => {
+    expect(service.showSidebar()).toBe(true);
+  });
 });
