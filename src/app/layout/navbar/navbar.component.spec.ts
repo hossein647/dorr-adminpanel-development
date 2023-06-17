55d413aabe2dbaa866c97d1dde54daeaf0f36e6e
hossein <developandroo@gmail.com>
2023-06-17 17:16:03 +0330

    import MockLayoutService and commit som code unusable

--- src/app/layout/navbar/navbar.component.spec.ts
+++ src/app/layout/navbar/navbar.component.spec.ts
@@ -1,17 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { LayoutService, MockLayoutService } from '../layout.service';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
+  let layoutService = new LayoutService();
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [NavbarComponent]
+      imports: [NavbarComponent],
+      providers: [
+        { provide: LayoutService, useClass: MockLayoutService }
+      ]
     });
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
+    layoutService = TestBed.inject(LayoutService)
     fixture.detectChanges();
   });
 
