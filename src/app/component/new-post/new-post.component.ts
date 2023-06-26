2ca44984ad07ead8351bedd97a162991873fa37a
hossein <developandroo@gmail.com>
2023-06-26 18:02:19 +0330

    create homeComponent

---
+++ src/app/component/new-post/new-post.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-new-post',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './new-post.component.html',
+  styleUrls: ['./new-post.component.scss']
+})
+export class NewPostComponent {
+
+}
