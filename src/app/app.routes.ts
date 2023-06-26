53d472258965edec8c0410dbb91ca4584b1116d3
hossein <developandroo@gmail.com>
2023-06-26 17:58:45 +0330

    change all keyword layout component and directory and service to dashboard and create new route for homeComponent and newPostComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,33 @@
 import { Routes } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
     { 
-        path: '', loadComponent: () => import('./layout/layout.component').then((m) => m.LayoutComponent),
-        children: [],
+        path: '',
+        redirectTo: 'login',
         pathMatch: 'full' 
-    }
+    },
+    { 
+        path: 'login', 
+        loadComponent: () => import('./auth/login/login.component').then((m) => m.LoginComponent),
+        pathMatch: 'full' 
+    },
+    { 
+        path: 'register', 
+        loadComponent: () => import('./auth/register/register.component').then((m) => m.RegisterComponent),
+        pathMatch: 'full' 
+    },
+    { 
+        path: 'dashboard', 
+        loadComponent: () => import('./dashboard/dashboard.component').then((m) => m.DashboardComponent), 
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./component/home/home.component').then((c) => c.HomeComponent)
+            },
+            {
+                path: 'new-post',
+                loadComponent: () => import('./component/new-post/new-post.component').then((c) => c.NewPostComponent)
+            }
+        ]
+    },
 ];
