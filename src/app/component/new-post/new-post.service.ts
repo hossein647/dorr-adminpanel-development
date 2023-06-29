41bcd1601db8ed23d244cab8c9f0e831d1ab5597
hossein <developandroo@gmail.com>
2023-06-29 12:44:13 +0330

    add create methode for new post

--- src/app/component/new-post/new-post.service.ts
+++ src/app/component/new-post/new-post.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from 'src/app/interfaces/post.interface';
+import { GenericHttp } from 'src/app/shared/generic-http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewPostService {
 
-  constructor() { }
+  baseApi = environment.url;
+
+  constructor(
+    private genericHttp: GenericHttp,
+  ) { }
+
+
+  create<Post>(data: Post): Observable<Post> {
+    return this.genericHttp.post(`${this.baseApi}/create`, data);
+  }
+
+
+  
 }
