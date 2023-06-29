e0085cf150fdb90d055ab097470de7134cf7e1a4
hossein <developandroo@gmail.com>
2023-06-29 12:18:40 +0330

    create generic HttpsService for handle Service http

---
+++ src/app/shared/http-service.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpServiceService {
+
+  constructor(private http: HttpClient) { }
+
+
+  get<T>(url: string): Observable<T> {
+    return this.http.get<T>(url)
+      .pipe(
+        catchError(err => throwError(() => err))
+      )
+  }
+  
+  
+  getAll<T>(url: string): Observable<T> {
+    return this.http.get<T>(url)
+      .pipe(
+        catchError(err => throwError(() => err))
+      )
+  }
+
+
+  post<T>(url: string, payload: any): Observable<T> {
+    return this.http.post<T>(url, payload)
+      .pipe(
+        catchError(err => throwError(() => err))
+      )
+  }
+
+
+  put<T>(url: string, payload: any): Observable<T> {
+    return this.http.put<T>(url, payload)
+      .pipe(
+        catchError(err => throwError(() => err))
+      )
+  }
+
+
+  delete<T>(url: string): Observable<T> {
+    return this.http.delete<T>(url)
+      .pipe(
+        catchError(err => throwError(() => err))
+      )
+  }
+}
