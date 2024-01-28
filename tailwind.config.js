fba251f445f2d3075c24aad324772dfadca8bc24
Hossein <hosseinnejaty2@gmail.com>
2024-01-28 22:33:18 +0330

    add xs breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "xs": '360px',
+        ...this.theme.screens
+      }
+    },
   },
   plugins: [],
 }
