684708618f0304de8be7a3f402d999a3548ea650
Hossein <hosseinnejaty2@gmail.com>
2024-01-31 13:20:01 +0330

    remove old route not used in new project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -31,42 +31,6 @@ export const routes: Routes = [
                 loadComponent: () => import('./component/home/home.component').then((c) => c.HomeComponent),
                 pathMatch: 'full',
             },
-            {
-                path: 'articles',
-                loadComponent: () => import('./component/article/home-article/home-article.component').then((c) => c.HomeArticleComponent),
-                children: [
-                    {
-                        path: '',
-                        loadComponent: () => import('./component/article/articles/articles.component').then((c) => c.ArticlesComponent),
-                    },
-                    {
-                        path: 'new',
-                        loadComponent: () => import('./component/article/new-article/new-article.component').then((c) => c.NewArticleComponent),
-                    },
-                    {
-                        path: 'edit',
-                        loadComponent: () => import('./component/article/new-article/new-article.component').then((c) => c.NewArticleComponent),
-                    },
-                ]
-            },
-            {
-                path: 'categories',
-                loadComponent: () => import('./component/category/home-category/home-category.component').then((c) => c.HomeCategoryComponent),
-                children: [
-                    {
-                        path: '',
-                        loadComponent: () => import('./component/category/categories/categories.component').then((c) => c.CategoriesComponent),
-                    },
-                    {
-                        path: 'new',
-                        loadComponent: () => import('./component/category/new-category/new-category.component').then((c) => c.NewCategoryComponent),
-                    },
-                    {
-                        path: 'edit',
-                        loadComponent: () => import('./component/category/new-category/new-category.component').then((c) => c.NewCategoryComponent),
-                    },
-                ]
-            },
         ]
     },
     {
