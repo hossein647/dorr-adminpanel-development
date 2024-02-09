68265c5e01215294ef9d538c09f1c6fd425d87b3
Hossein <hosseinnejaty2@gmail.com>
2024-02-09 22:06:31 +0330

    make CreateCategoryComponent and ReadCategoriesComponent and add relative route

---
+++ src/app/menu/categories/read-categories/read-categories.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-read-categories',
+  standalone: true,
+  imports: [],
+  templateUrl: './read-categories.component.html',
+  styleUrl: './read-categories.component.scss'
+})
+export class ReadCategoriesComponent {
+
+}
