e9b955e708f4ac9ec73dd1755af55cc5fe718094
Hossein <hosseinnejaty2@gmail.com>
2024-02-09 08:45:26 +0330

    import ButtonModule, InputTextModule, RouterLink

--- src/app/menu/books/read-book/read-book.component.ts
+++ src/app/menu/books/read-book/read-book.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-read-book',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, InputTextModule, RouterLink],
   templateUrl: './read-book.component.html',
   styleUrl: './read-book.component.scss'
 })
