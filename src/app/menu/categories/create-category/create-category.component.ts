7cee278a57a958ca0e9f1662c28c5159eca13d33
omid <omidebrahimi064@gmail.com>
2024-02-10 17:49:04 +0330

    import InputTextModule, ButtonModule, CreateBookComponent to CreateCategoryComponent

--- src/app/menu/categories/create-category/create-category.component.ts
+++ src/app/menu/categories/create-category/create-category.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CreateBookComponent } from '../../books/create-book/create-book.component';
 
 @Component({
   selector: 'app-create-category',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule, ButtonModule, CreateBookComponent],
   templateUrl: './create-category.component.html',
   styleUrl: './create-category.component.scss'
 })
