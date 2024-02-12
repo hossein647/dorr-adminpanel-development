82731e7df621c450eef700e4f09de6ba888cc040
Hossein <hosseinnejaty2@gmail.com>
2024-02-12 15:12:24 +0330

    implement ICrud<T> interface by CrudService<T> and init relative methods

--- src/app/services/core/crud.service.ts
+++ src/app/services/core/crud.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ICrud } from 'src/app/shared/interfaces/i-crud.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CrudService {
+export class CrudService<T> implements ICrud<T> {
 
   constructor() { }
+  create(item: T): Observable<T> {
+    throw new Error('Method not implemented.');
+  }
+  update(id: string, item: T): Observable<T> {
+    throw new Error('Method not implemented.');
+  }
+  delete(id: string): Observable<T> {
+    throw new Error('Method not implemented.');
+  }
+  get(id: string): Observable<T> {
+    throw new Error('Method not implemented.');
+  }
+  getAll(): Observable<T[]> {
+    throw new Error('Method not implemented.');
+  }
 }
