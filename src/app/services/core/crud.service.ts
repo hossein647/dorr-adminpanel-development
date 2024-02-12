9e7b2f4edca2d6264fdb7f8bfc1fe9a8e66cd213
Hossein <hosseinnejaty2@gmail.com>
2024-02-12 15:14:42 +0330

    add HttpClient and baseApi property

--- src/app/services/core/crud.service.ts
+++ src/app/services/core/crud.service.ts
@@ -1,13 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ICrud } from 'src/app/shared/interfaces/i-crud.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService<T> implements ICrud<T> {
 
-  constructor() { }
+  baseApi: string = environment.url;
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+
   create(item: T): Observable<T> {
     throw new Error('Method not implemented.');
   }
