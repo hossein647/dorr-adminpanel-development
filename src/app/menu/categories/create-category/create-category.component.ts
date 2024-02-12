32e4a779f7d1ec78571d99cd7b9cc451c09c2d13
Hossein <hosseinnejaty2@gmail.com>
2024-02-12 16:34:34 +0330

    import CrudService to provider

--- src/app/menu/categories/create-category/create-category.component.ts
+++ src/app/menu/categories/create-category/create-category.component.ts
@@ -3,11 +3,13 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { CrudService } from 'src/app/services/core/crud.service';
 
 @Component({
   selector: 'app-create-category',
   standalone: true,
   imports: [InputTextModule, ButtonModule, ReactiveFormsModule, CommonModule],
+  providers: [CrudService],
   templateUrl: './create-category.component.html',
   styleUrl: './create-category.component.scss'
 })
