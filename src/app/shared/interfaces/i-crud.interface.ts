0cc17d136d9db1419a95597f7663b2c224587e99
Hossein <hosseinnejaty2@gmail.com>
2024-02-12 15:30:01 +0330

    add endPoint and options parameter to another methods and change type options to optional

--- src/app/shared/interfaces/i-crud.interface.ts
+++ src/app/shared/interfaces/i-crud.interface.ts
@@ -3,13 +3,13 @@ import { Observable } from "rxjs";
 
 export interface ICrud<T> {
 
-    create(item: T, endPoint: string, options: any): Observable<HttpEvent<T>>;
+    create(item: T, endPoint: string, options?: any): Observable<HttpEvent<T>>;
 
-    update(id: string, item: T): Observable<HttpEvent<T>>;
+    update(id: string, item: T, endPoint: string, options?: any): Observable<HttpEvent<T>>;
 
-    delete(id: string): Observable<HttpEvent<T>>;
+    delete(id: string, endPoint: string, options?: any): Observable<HttpEvent<T>>;
 
-    get(id: string): Observable<HttpEvent<T>>;
+    get(id: string, endPoint: string, options?: any): Observable<HttpEvent<T>>;
 
-    getAll(): Observable<HttpEvent<T[]>>;
+    getAll(endPoint: string, options?: any): Observable<HttpEvent<T[]>>;
 }
