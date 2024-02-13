d56495269d2984d43cf358fdfd982457f9959d69
Hossein <hosseinnejaty2@gmail.com>
2024-02-13 18:32:25 +0330

    handle part of validation in ts file, and complete implemention submitCategory

--- src/app/menu/categories/create-category/create-category.component.ts
+++ src/app/menu/categories/create-category/create-category.component.ts
@@ -4,12 +4,15 @@ import { ButtonModule } from 'primeng/button';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { CrudService } from 'src/app/services/core/crud.service';
+import { Category } from 'src/app/shared/interfaces/models/category.interface';
+import { MessageService } from 'primeng/api';
+import { ToastComponent } from 'src/app/shared/component/toast/toast.component';
 
 @Component({
   selector: 'app-create-category',
   standalone: true,
-  imports: [InputTextModule, ButtonModule, ReactiveFormsModule, CommonModule],
-  providers: [CrudService],
+  imports: [InputTextModule, ButtonModule, ReactiveFormsModule, CommonModule, ToastComponent],
+  providers: [CrudService, MessageService],
   templateUrl: './create-category.component.html',
   styleUrl: './create-category.component.scss'
 })
@@ -20,7 +23,9 @@ export class CreateCategoryComponent implements OnInit {
   loading: boolean = false;
   
   constructor(
-    private foromBuilder: FormBuilder
+    private foromBuilder: FormBuilder,
+    private crudService: CrudService<Category>,
+    private messageService: MessageService
   ) {}
 
 
@@ -39,7 +44,27 @@ export class CreateCategoryComponent implements OnInit {
 
 
   submitCategory(categoryForm: FormGroup) {
-    console.log(categoryForm.value);
+    if (categoryForm.invalid) return categoryForm.markAllAsTouched();
+
+    const endPoint = 'category/create'
     this.loading = true;
+
+    this.crudService.create(categoryForm.value, endPoint).subscribe({
+      next: (res: any) => {
+        this.loading = false;
+        this.messageService.add({ severity: 'success', summary: res.message})
+        this.categoryForm.reset();
+      },
+      error: (err: any) => {
+        this.loading = false;
+        this.messageService.add({ severity: 'error', summary: err.error.message })
+      }
+    })
+  }
+
+
+  isEmptyInput(formControlName: string): boolean {
+    const input = this.categoryForm.controls[formControlName];    
+    return (input.invalid && (input.touched || input.dirty))
   }
 }
