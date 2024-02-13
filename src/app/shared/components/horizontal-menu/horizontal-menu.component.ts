e3c98637ded3ac4b20b7ecf028e0e2a9ba47f62e
Hossein <hosseinnejaty2@gmail.com>
2024-02-13 21:40:05 +0330

    define input type signal for createRouter, editRouter, createLabel, editLabel

--- src/app/shared/components/horizontal-menu/horizontal-menu.component.ts
+++ src/app/shared/components/horizontal-menu/horizontal-menu.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-horizontal-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './horizontal-menu.component.html',
   styleUrl: './horizontal-menu.component.scss'
 })
 export class HorizontalMenuComponent {
 
+  createRouter = input.required<string>();
+  editRouter   = input.required<string>();
+
+  createLabel  = input.required<string>();
+  editLabel    = input.required<string>();
 }
