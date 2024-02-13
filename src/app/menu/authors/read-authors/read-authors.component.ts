a6f0d3bd0f8c6cbccf84959122bdcce2fb74d8aa
omid <omidebrahimi064@gmail.com>
2024-02-13 22:37:10 +0330

    make ReadAuthorsComponent component

---
+++ src/app/menu/authors/read-authors/read-authors.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-read-authors',
+  standalone: true,
+  imports: [],
+  templateUrl: './read-authors.component.html',
+  styleUrl: './read-authors.component.scss'
+})
+export class ReadAuthorsComponent {
+
+}
