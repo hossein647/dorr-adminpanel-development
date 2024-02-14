55b6c13db4c1cafa0049103bd496c333c65f51b5
Hossein <hosseinnejaty2@gmail.com>
2024-02-14 09:39:45 +0330

    import isEmptyInput valdiation and crate method for return this value and init class CreateAuthorComponent and import formbuilder

--- src/app/menu/authors/create-author/create-author.component.ts
+++ src/app/menu/authors/create-author/create-author.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { isEmptyInput } from 'src/app/shared/validations/empty.validation';
 
 @Component({
   selector: 'app-create-author',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './create-author.component.html',
   styleUrl: './create-author.component.scss'
 })
-export class CreateAuthorComponent {
+export class CreateAuthorComponent implements OnInit {
 
+
+  authorForm: FormGroup;
+  
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {}
+
+
+  ngOnInit(): void {}
+
+
+
+  initForm() {}
+
+  submitAuthor(authorForm: FormGroup) {}
+
+
+  isEmptyInput(formControlName: string): boolean {
+    return isEmptyInput(this.authorForm, formControlName)
+  } 
 }
