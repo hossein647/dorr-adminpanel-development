15bf4ae74e7b1c43d4728072cb958156758f288e
Hossein <hosseinnejaty2@gmail.com>
2024-02-14 17:18:26 +0330

    defint loading, authorData, countries property and call initForm and init authorData and reaname RequiredValidtors to Validators.required and init getAuhorData method

--- src/app/menu/authors/create-author/create-author.component.ts
+++ src/app/menu/authors/create-author/create-author.component.ts
@@ -1,12 +1,14 @@
 import { NgClass } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { MultiSelectModule } from 'primeng/multiselect'; 
 import { isEmptyInput } from 'src/app/shared/validations/empty.validation';
 
 @Component({
   selector: 'app-create-author',
   standalone: true,
-  imports: [ReactiveFormsModule, NgClass],
+  imports: [ReactiveFormsModule, NgClass, MultiSelectModule, ButtonModule],
   templateUrl: './create-author.component.html',
   styleUrl: './create-author.component.scss'
 })
@@ -14,6 +16,9 @@ export class CreateAuthorComponent implements OnInit {
 
 
   authorForm: FormGroup;
+  loading: boolean = false;
+  authorData: any[];
+  countries: [] = []
   
 
   constructor(
@@ -21,16 +26,52 @@ export class CreateAuthorComponent implements OnInit {
   ) {}
 
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initForm();
+    this.authorData = this.getAuthorData();
+  }
 
 
 
-  initForm() {}
+  initForm() {
+    this.authorForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      birthDate: ['', Validators.required],
+      deathDate: ['', Validators.required],
+      book: [undefined, Validators.required],
+    })
+  }
 
   submitAuthor(authorForm: FormGroup) {}
+
 
 
   isEmptyInput(formControlName: string): boolean {
     return isEmptyInput(this.authorForm, formControlName)
   } 
+
+
+
+  getAuthorData(): any[] {
+    return [
+      {
+        formControlName: 'name',
+        placeholder: 'نام نویسنده',
+        for: 'name',
+        id: 'name',
+      },
+      {
+        formControlName: 'birthDate',
+        placeholder: 'سال تولد',
+        for: 'birthDate',
+        id: 'birthDate',
+      },
+      {
+        formControlName: 'deathDate',
+        placeholder: 'سال وفات',
+        for: 'deathDate',
+        id: 'deathDate',
+      },
+    ]
+  }
 }
