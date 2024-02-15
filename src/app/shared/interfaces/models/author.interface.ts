6ffc7622f23ca0a9099a44a8fac337b723bf9ce7
Hossein <hosseinnejaty2@gmail.com>
2024-02-15 13:11:52 +0330

    reanme type formControlName to profession and add p-selectButton for language property

--- src/app/shared/interfaces/models/author.interface.ts
+++ src/app/shared/interfaces/models/author.interface.ts
@@ -1,6 +1,7 @@
 export interface Author {
     name: string;
     books?: string[];
+    language: string;
     birthDate: string;
     deathData: string;
     profession: 'Author' | 'Translator' | 'Both'
