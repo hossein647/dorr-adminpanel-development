613d1df0d08969eaac0dd2693bbb61f27810bbbe
Hossein <hosseinnejaty2@gmail.com>
2024-02-15 11:56:38 +0330

    import SelectButtonModule and define typeAuthor and init value and add to initForm change placeholder's name input

--- src/app/menu/authors/create-author/create-author.component.ts
+++ src/app/menu/authors/create-author/create-author.component.ts
@@ -6,12 +6,13 @@ import { MultiSelectModule } from 'primeng/multiselect';
 import { CrudService } from 'src/app/services/core/crud.service';
 import { Author } from 'src/app/shared/interfaces';
 import { isEmptyInput } from 'src/app/shared/validations';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 
 @Component({
   selector: 'app-create-author',
   standalone: true,
-  imports: [ReactiveFormsModule, NgClass, MultiSelectModule, ButtonModule],
+  imports: [ReactiveFormsModule, NgClass, MultiSelectModule, ButtonModule, SelectButtonModule],
   providers: [CrudService],
   templateUrl: './create-author.component.html',
   styleUrl: './create-author.component.scss'
@@ -23,6 +24,7 @@ export class CreateAuthorComponent implements OnInit {
   loading: boolean = false;
   authorData: any[];
   countries: [] = []
+  typeAuthor: any[];
   
 
   constructor(
@@ -34,6 +36,11 @@ export class CreateAuthorComponent implements OnInit {
   ngOnInit(): void {
     this.initForm();
     this.authorData = this.getAuthorData();
+    this.typeAuthor = [
+      { label: 'نویسنده', value: 'author' }, 
+      { label: 'مترجم', value: 'translator' },
+      { label: 'هر دو', value: 'both' }, 
+    ]
   }
 
 
@@ -43,7 +50,8 @@ export class CreateAuthorComponent implements OnInit {
       name: ['', Validators.required],
       birthDate: ['', Validators.required],
       deathDate: ['', Validators.required],
-      book: [undefined],
+      books: [undefined],
+      type: ''
     })
   }
 
@@ -80,7 +88,7 @@ export class CreateAuthorComponent implements OnInit {
       {
         label: 'نام :',
         formControlName: 'name',
-        placeholder: 'نام نویسنده',
+        placeholder: 'نام نویسنده یا مترجم',
         for: 'name',
         id: 'name',
         error: 'فیلد نام را پر کنید.'
