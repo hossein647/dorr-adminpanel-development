f833a3bc80052da577da8755eb2d388cb696eed8
Hossein <hosseinnejaty2@gmail.com>
2024-02-16 23:41:27 +0330

    rename validaton file empty.validation.ts to is-empty-validation.ts

--- src/app/shared/validations/empty.validation.ts
+++
@@ -1,6 +0,0 @@
-import { FormGroup } from "@angular/forms"
-
-export const isEmptyInput = (formGroup: FormGroup, formControlName: string): boolean => {
-    const input = formGroup.controls[formControlName];
-    return (input.invalid && (input.touched || input.dirty));
-}
