f9c2327cb90ff1529bec5ad3a710e6cdfbe9efdc
omid <omidebrahimi064@gmail.com>
2024-02-29 12:35:01 +0330

    create my-table for use in  another component

---
+++ src/app/shared/components/my-table/my-table.component.ts
@@ -0,0 +1,51 @@
+import { Component, Input, input } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { Book, CustomBook } from 'src/app/shared/interfaces';
+
+interface Column {
+  field: string;
+  header: string;
+}
+
+@Component({
+  selector: 'app-my-table',
+  standalone: true,
+  imports: [TableModule],
+  templateUrl: './my-table.component.html',
+  styleUrl: './my-table.component.scss'
+})
+export class MyTableComponent {
+  // rows = input.required<CustomBook>();
+  // cols = input.required<Column>();
+  @Input() data : CustomBook[]
+  @Input() cols : Column[]
+
+  ngOnInit() {
+    // this.cols([
+    //   { field: 'name', header: 'نام کتاب' },
+    //   { field: 'author', header: 'نویسنده' },
+    //   { field: 'category', header: 'دسته بندی' },
+    //   { field: 'publisher', header: 'انتشارات' }
+    // ]) 
+
+    // this.rows = [
+    //   {[
+    //   { field: 'name', header: 'نام کتاب' },
+    //   { field: 'author', header: 'نویسنده' },
+    //   { field: 'category', header: 'دسته بندی' },
+    //   { field: 'publisher', header: 'انتشارات' }
+    // ]
+    //     name: 'فارسی',
+    //     author: 'سعدی',
+    //     category: 'ادبیات',
+    //     publisher: 'بیان'
+    //   },
+    //   {
+    //     name: 'ریاضی',
+    //     author: 'حوارزمی',
+    //     category: 'ریاضیات',
+    //     publisher: 'خودم'
+    //   }
+    // ]
+  }
+}
