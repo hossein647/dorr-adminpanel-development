c03f2a6f7a11d7b5b5da0a6fec9dca1adf1b7b9d
omid <omidebrahimi064@gmail.com>
2024-02-29 18:02:57 +0330

    import HorizontalMenuComponent and ini roperties of HorizontalMenuComponent

--- src/app/menu/books/read-book/read-book.component.ts
+++ src/app/menu/books/read-book/read-book.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { HorizontalMenuComponent } from 'src/app/shared/components/horizontal-menu/horizontal-menu.component';
 import { MyTableComponent } from 'src/app/shared/components/my-table/my-table.component';
 import { Book, CustomBook } from 'src/app/shared/interfaces';
 import { Column } from 'src/app/shared/interfaces/models/column.interface';
@@ -9,12 +10,17 @@ import { Column } from 'src/app/shared/interfaces/models/column.interface';
 @Component({
   selector: 'app-read-book',
   standalone: true,
-  imports: [ButtonModule, InputTextModule, RouterLink, MyTableComponent],
+  imports: [ButtonModule, InputTextModule, RouterLink, MyTableComponent, HorizontalMenuComponent],
   templateUrl: './read-book.component.html',
   styleUrl: './read-book.component.scss'
 })
 
 export class ReadBookComponent {
+  createLabel: string = 'ایجاد';
+  editLabel: string = 'ویرایش';
+  createRouter: string = 'create';
+  editRouter: string = '';
+
   books: CustomBook[];
 
   cols: Column[];
