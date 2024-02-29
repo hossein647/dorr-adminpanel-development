e9f51e6678bed8b3bb006a217de849562b9d29ef
omid <omidebrahimi064@gmail.com>
2024-02-29 20:42:27 +0330

    import MyTableComponent, CustomCategory and Column interface and init cols and rows

--- src/app/menu/categories/read-categories/read-categories.component.ts
+++ src/app/menu/categories/read-categories/read-categories.component.ts
@@ -3,22 +3,44 @@ import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { HorizontalMenuComponent } from 'src/app/shared/components/horizontal-menu/horizontal-menu.component';
+import { MyTableComponent } from 'src/app/shared/components/my-table/my-table.component';
+import { CustomCategory } from 'src/app/shared/interfaces/models/category.interface';
+import { Column } from 'src/app/shared/interfaces/models/column.interface';
 
 @Component({
   selector: 'app-read-categories',
   standalone: true,
-  imports: [ButtonModule, InputTextModule, RouterLink, HorizontalMenuComponent],
+  imports: [ButtonModule, InputTextModule, RouterLink, HorizontalMenuComponent, MyTableComponent],
   templateUrl: './read-categories.component.html',
   styleUrl: './read-categories.component.scss'
 })
+
 export class ReadCategoriesComponent implements OnInit {
-  
   createLabel : string = 'ایجاد';
   editLabel   : string = 'ویرایش';
   createRouter: string = 'create';
   editRouter  : string = '';
 
+  categories: CustomCategory[];
+  cols: Column[];
 
-  ngOnInit(): void {}
+
+  ngOnInit() {
+    this.cols = [
+      { field: 'name', header: 'نام' },
+      { field: 'alias', header: 'مستعار' }
+    ]
+
+    this.categories = [
+      {
+        name: 'فارسی',
+        alias: 'سعدی'
+      },
+      {
+        name: 'ریاضی',
+        alias: 'حوارزمی'
+      },
+    ]
+  }
     
 }
