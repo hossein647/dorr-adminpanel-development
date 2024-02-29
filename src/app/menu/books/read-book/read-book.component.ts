3989ab120af8d8fab6cf13276acff6adddd3ec47
omid <omidebrahimi064@gmail.com>
2024-02-29 12:38:18 +0330

    import MyTableComponent and CustomBook interface

--- src/app/menu/books/read-book/read-book.component.ts
+++ src/app/menu/books/read-book/read-book.component.ts
@@ -2,14 +2,48 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { MyTableComponent } from 'src/app/shared/components/my-table/my-table.component';
+import { Book, CustomBook } from 'src/app/shared/interfaces';
+
+interface Column {
+  field: string;
+  header: string;
+}
 
 @Component({
   selector: 'app-read-book',
   standalone: true,
-  imports: [ButtonModule, InputTextModule, RouterLink],
+  imports: [ButtonModule, InputTextModule, RouterLink, MyTableComponent],
   templateUrl: './read-book.component.html',
   styleUrl: './read-book.component.scss'
 })
-export class ReadBookComponent {
 
+export class ReadBookComponent {
+  books: CustomBook[];
+
+  cols: Column[];
+
+  ngOnInit() {
+    this.cols = [
+      { field: 'name', header: 'نام کتاب' },
+      { field: 'author', header: 'نویسنده' },
+      { field: 'category', header: 'دسته بندی' },
+      { field: 'publisher', header: 'انتشارات' }
+    ]
+
+    this.books = [
+      {
+        name: 'فارسی',
+        author: 'سعدی',
+        category: 'ادبیات',
+        publisher: 'بیان'
+      },
+      {
+        name: 'ریاضی',
+        author: 'حوارزمی',
+        category: 'ریاضیات',
+        publisher: 'خودم'
+      }
+    ]
+  }
 }
