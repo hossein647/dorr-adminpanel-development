e2ee3488cf0bb4f74cef2a5b8fbdb79dd59489ae
omid <omidebrahimi064@gmail.com>
2024-02-29 14:02:28 +0330

    MyTableComponent, CustomAuthor and Column interface and init cols and authors

--- src/app/menu/authors/read-authors/read-authors.component.ts
+++ src/app/menu/authors/read-authors/read-authors.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { HorizontalMenuComponent } from 'src/app/shared/components/horizontal-menu/horizontal-menu.component';
+import { MyTableComponent } from 'src/app/shared/components/my-table/my-table.component';
+import { CustomAuthor } from 'src/app/shared/interfaces';
+import { Column } from 'src/app/shared/interfaces/models/column.interface';
 
 @Component({
   selector: 'app-read-authors',
   standalone: true,
-  imports: [HorizontalMenuComponent],
+  imports: [HorizontalMenuComponent, MyTableComponent],
   templateUrl: './read-authors.component.html',
   styleUrl: './read-authors.component.scss'
 })
@@ -13,4 +16,29 @@ export class ReadAuthorsComponent {
   editLabel: string = 'ویرایش';
   createRouter: string = 'create';
   editRouter: string = '';
+
+  authors: CustomAuthor[];
+
+  cols: Column[];
+
+  ngOnInit() {
+    this.cols = [
+      { field: 'name', header: 'نام نویسنده' },
+      { field: 'birthDate', header: 'تاریخ تولد' },
+      { field: 'profession', header: 'تخصص' }
+    ]
+
+    this.authors = [
+      {
+        name: 'حسین',
+        birthDate: '1935',
+        profession: "Author"
+      },
+      {
+        name: 'حسین',
+        birthDate: '1935',
+        profession: "Author"
+      }
+    ]
+  }
 }
