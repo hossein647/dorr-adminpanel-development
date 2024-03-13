d383876dc3bbab20259491573a8d40cc12deac37
omid <omidebrahimi064@gmail.com>
2024-03-13 14:05:06 +0330

    send request to data base to get all categories

--- src/app/menu/categories/read-categories/read-categories.component.ts
+++ src/app/menu/categories/read-categories/read-categories.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { firstValueFrom } from 'rxjs';
 import { HorizontalMenuComponent } from 'src/app/shared/components/horizontal-menu/horizontal-menu.component';
 import { MyTableComponent } from 'src/app/shared/components/my-table/my-table.component';
-import { CustomCategory } from 'src/app/shared/interfaces/models/category.interface';
+import { Category, CustomCategory } from 'src/app/shared/interfaces/models/category.interface';
 import { Column } from 'src/app/shared/interfaces/models/column.interface';
+import { CategoryService } from 'src/app/shared/services/category.service';
 
 @Component({
   selector: 'app-read-categories',
@@ -20,16 +22,29 @@ export class ReadCategoriesComponent implements OnInit {
   editLabel   : string = 'ویرایش';
   createRouter: string = 'create';
   editRouter  : string = '';
-
   categories: CustomCategory[] = [];
   cols: Column[];
 
+  constructor(
+    private categoriesService: CategoryService<Category>
+  ) { }
 
   ngOnInit() {
     this.cols = [
       { field: 'name', header: 'نام' },
       { field: 'alias', header: 'مستعار' }
-    ]
+    ];
+
+    this.getCategories();
   }
-    
+  
+
+  async getCategories() {
+    const request = this.categoriesService.getAll('category/getAll', false)
+    const response: any = await (firstValueFrom(request))
+
+    console.log(request);
+
+    this.categories = response.data
+  }
 }
